31727fccc9fbc98fa94deb5bb41f28dda6d356b8
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-03-04 14:02:58 -0700

    fix local db config

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection
-if(process.env.JAWSDB_URL){
+var connection;
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: b5xi70i95nih7sbk,
-  password: yii0vxyanbeho1kr,
-  database: dxbll5nlkxiruxy9
-});
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 }
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
